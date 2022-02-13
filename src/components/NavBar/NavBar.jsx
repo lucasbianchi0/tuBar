420240c7c502605ace2311e6c70142f4ab492a1d
Lucas Bianchi <lucmbianchi2000@gmail.com>
2022-02-13 20:12:17 -0300

    se agrego carrito e ItemListContainer con props greeting

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import './NavBar.css';
+import CartWidget from '../Ui/Atoms/CartWidget/CartWidget';
 
 
 const NavBar = () => {
   return (
     <div className='container-nav'>
       <div className='estructura-Navbar'>
-          <div>
-              <p>TuBar.com</p>
-          </div>
-        
+        <div>
+            <p>TuBar.com</p>
+        </div>
+        {/* derecha */}
+        <div className='navbar-right'>
           <ul>
               <a href="/">Catalogo</a>
               <a href="/">Reservas</a>
               <a href="/">Nosotros</a>
           </ul>
+          <div><CartWidget /></div>
+        </div>
         </div>
     </div>
   )
